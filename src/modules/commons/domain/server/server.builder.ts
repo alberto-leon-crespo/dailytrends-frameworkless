ff84684e1d20bde699897acc1ee05804dc5078e3
Alberto León <alberto.leon.crespo@gmail.com>
2023-08-03 10:57:30 +0200

    feat: modified server build to be more friendly with async startups

--- src/modules/commons/domain/server/server.builder.ts
+++ src/modules/commons/domain/server/server.builder.ts
@@ -1,21 +1,36 @@
-import {InversifyExpressServer, RoutingConfig} from "inversify-express-utils";
+import { InversifyExpressServer, RoutingConfig } from "inversify-express-utils";
 import * as bodyParser from "body-parser";
 import expressPino from "pino-http";
-import {Container} from "inversify";
+import { Container } from "inversify";
+import {LoggerService} from "../services/logger.service";
+import {ConfigService} from "../services/config.service";
+import {CommonsModule} from "../../commons.module";
+import * as express from 'express';
 
 export class ServerBuilder {
-    public static build(container: Container, routingConfig: RoutingConfig) {
-        const server = new InversifyExpressServer(container, null, routingConfig);
+    public static build(container: Container, routingConfig: RoutingConfig): Promise<express.Application> {
+        return new Promise((resolve: any, reject: any) => {
+            const server = new InversifyExpressServer(container, null, routingConfig);
 
-        server.setConfig((app) => {
-            // add body parser
-            app.use(bodyParser.urlencoded({
-                extended: true
-            }));
-            app.use(bodyParser.json());
-            app.use(expressPino());
-        });
+            server.setConfig((app) => {
+                // add body parser
+                app.use(bodyParser.urlencoded({
+                    extended: true
+                }));
+                app.use(bodyParser.json());
+                app.use(expressPino());
+            });
 
-        return server.build();
+            const logger = container.get<LoggerService>(LoggerService);
+            const configService = container.get<ConfigService>(ConfigService);
+            const port = configService.getModuleConfig(CommonsModule.name, 'app.port', 3000);
+
+            const serverInstance = server.build();
+
+            serverInstance.listen(port, 'localhost', () => {
+                logger.info(`Server started on port ${port} :)`);
+                resolve(serverInstance);
+            }).on('error', reject);
+        })
     }
 }
